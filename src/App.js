5ada9b9cf130775cce42b404c02cd4fc408bd954
Victor Rivera <verm2405@gmail.com>
2018-11-09 23:59:17 -0500

    Getting the Layout of the App how I want so I can start working on the components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { Row, Col, Layout } from "antd";
 
 class App extends Component {
   render() {
+    const { Content } = Layout;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <Layout style={{ minHeight: "100vh" }}>
+          <Content style={{ marginTop: "100px" }}>
+            <Row type="flex" justify="space-around" align="middle">
+              <Col
+                style={{ textAlign: "center", background: "#ddd" }}
+                span={16}
+              >
+                Search Component
+              </Col>
+            </Row>
+            <Row type="flex" justify="center" align="middle">
+              <Col
+                style={{ textAlign: "center", background: "green" }}
+                span={6}
+              >
+                Image
+              </Col>
+              <Col style={{ textAlign: "center", background: "red" }} span={10}>
+                Movie Details
+              </Col>
+            </Row>
+          </Content>
+        </Layout>
+      </Fragment>
     );
   }
 }
