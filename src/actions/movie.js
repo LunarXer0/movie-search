966d0683dabc91dde2094deb76977ca0f59de0e1
Victor Rivera <verm2405@gmail.com>
2018-11-15 21:53:53 -0500

    Converted actions to use arrow functions

--- src/actions/movie.js
+++ src/actions/movie.js
@@ -5,42 +5,36 @@ import {
   GET_MOVIE_FROM_OPTION
 } from "../reducers/movie";
 
-export function loadInitialMovie() {
-  return async function(dispatch) {
-    const res = await fetch(
-      `https://api.themoviedb.org/3/movie/157336?api_key=${API_KEY}&language=en-US`
-    );
-    const movie = await res.json();
-    console.log(movie);
-    return dispatch({
-      type: LOAD_INITIAL_MOVIE,
-      data: movie
-    });
-  };
-}
+export const loadInitialMovie = () => async dispatch => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/157336?api_key=${API_KEY}&language=en-US`
+  );
+  const movie = await res.json();
+  console.log(movie);
+  return dispatch({
+    type: LOAD_INITIAL_MOVIE,
+    data: movie
+  });
+};
 
-export function searchMovies(title) {
-  return async function(dispatch) {
-    const res = await fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${title}&page=1&include_adult=false`
-    );
-    const { results } = await res.json();
-    return dispatch({
-      type: SEARCH_MOVIE,
-      data: results
-    });
-  };
-}
+export const searchMovies = title => async dispatch => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${title}&page=1&include_adult=false`
+  );
+  const { results } = await res.json();
+  return dispatch({
+    type: SEARCH_MOVIE,
+    data: results
+  });
+};
 
-export function getOption(id) {
-  return async function(dispatch) {
-    const res = await fetch(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`
-    );
-    const movie = await res.json();
-    return dispatch({
-      type: GET_MOVIE_FROM_OPTION,
-      data: movie
-    });
-  };
-}
+export const getOption = id => async dispatch => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`
+  );
+  const movie = await res.json();
+  return dispatch({
+    type: GET_MOVIE_FROM_OPTION,
+    data: movie
+  });
+};
